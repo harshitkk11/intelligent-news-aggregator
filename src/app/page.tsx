c98fb1093246c0e11cc99e28473e4a365cb6399c
Harshit Kumar <114001877+harshitkk11@users.noreply.github.com>
2025-04-07 23:35:28 +0530

    Add navigation buttons for login and signup on the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
@@ -11,6 +14,14 @@ export default function Home() {
           View the source code on GitHub
         </a>
       </p>
+      <div className="flex items-center gap-3">
+        <Link href="/login" className="cursor-pointer">
+          <Button>Login</Button>
+        </Link>
+        <Link href="/signup" className="cursor-pointer">
+          <Button>Sign up</Button>
+        </Link>
+      </div>
     </main>
   );
 }
