a0e0175fce7116848d3e4eb18d4017c9ad6b8166
Harshit Kumar <114001877+harshitkk11@users.noreply.github.com>
2025-04-27 19:29:49 +0530

    fix(email): refactor sendVerificationEmail function to improve error handling and streamline mail sending process

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,5 +1,5 @@
 import React from "react";
-import { transporter } from "@/lib/email";
+import { createTransporter } from "@/lib/email";
 import VerificationEmail from "@/../emails/verificationEmail";
 import { ApiResponse } from "@/types/apiResponse";
 import { render } from "@react-email/components";
@@ -9,32 +9,40 @@ export async function sendVerificationEmail(
   email: string,
   verificationCode: string
 ): Promise<ApiResponse> {
+  const transporter = await createTransporter();
   try {
     const emailHtml = render(
       React.createElement(VerificationEmail, { name, verificationCode })
     );
 
-    await transporter.sendMail(
-      {
-        from: process.env.EMAIL_USER,
-        to: email,
-        subject: "Welcome to Our Service!",
-        html: emailHtml,
-      },
-      function (error, info) {
-        if (error) {
-          console.log(error);
-          return { success: false, message: "Failed to send verification email" };
-        } else {
-          console.log("Email sent: " + info.response);
-          return {
-            success: true,
-            message: "Verification email sent successfully",
-          };
+    const mailResponse = await new Promise<ApiResponse>((resolve) => {
+      transporter.sendMail(
+        {
+          from: `INA <${process.env.EMAIL_USER}>`,
+          to: email,
+          subject: "Welcome to Our Service!",
+          html: emailHtml,
+        },
+        function (error, info) {
+          if (error) {
+            console.log("ERROR", error);
+            resolve({
+              success: false,
+              message: "Failed to send verification email",
+            });
+          } else {
+            console.log("Email sent: " + info.response);
+            resolve({
+              success: true,
+              message:
+                "Verification email sent successfully",
+            });
+          }
         }
-      }
-    );
-    return { success: true, message: "Verification email sent successfully" };
+      );
+    });
+
+    return mailResponse;
   } catch (error) {
     console.error("Failed to send verification email", error);
 
